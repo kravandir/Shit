99a066911ce7b41d84b350c1db2f4973b046135e
kravandir <79300121+kravandir@users.noreply.github.com>
2021-10-23 22:33:56 +0400

    Create heck.sh

---
+++ heck.sh
@@ -0,0 +1,12 @@
+cd ~
+cd ..
+echo "ща сделаю взлом ссылкууу"
+mkdir vzlom
+cd vzlom 
+ip a > hui
+echo "пиздец незначительный: cd: Нет такого файла или каталога: vk.html"
+echo "mama shluxa" > vk.html
+echo "zzzzzzzzzzzzzzzzzz" > zsh
+#MATb EbA/| ))))))))))))))))))))
+echo "localhost:8080 - ссылка"
+cat FUCK YOU > hehehe | rm - rf *
