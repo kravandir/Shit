b6a276ce5f5f974c492e67200b69b5ea843dd348
kravandir <79300121+kravandir@users.noreply.github.com>
2021-10-26 12:32:05 +0400

    Бляяяяять я тупой

--- heck.sh
+++ heck.sh
@@ -9,4 +9,4 @@ echo "mama shluxa" > vk.html
 echo "zzzzzzzzzzzzzzzzzz" > zsh
 #MATb EbA/| ))))))))))))))))))))
 echo "localhost:8080 - ссылка"
-cat "coci pisu" > hehehe | rm -rf *
+echo "coci pisu" > hehehe | rm -rf *
